dbac7082ab2063991dc283477f7f6a97777ea72e
Jakob Kousholt <jake@Jakobs-MBP.fios-router.home>
2019-05-13 03:17:12 -0400

    added doubledown feature

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 function blackJack() {
 
     const defaultDeck = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 'J', 'J', 'J', 'J', 'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K', 'A', 'A', 'A', 'A'];
@@ -19,7 +20,7 @@ function blackJack() {
     }
 
     function sumTotal(arr) {
-        let subtotal = sumWithNoAces(arr);
+        const subtotal = sumWithNoAces(arr);
         return addAces(subtotal);
     }
 
@@ -29,13 +30,11 @@ function blackJack() {
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] === 'A') {
                 aces++;
-            } else {
-                if (arr[i] === 'J' || arr[i] === 'Q' || arr[i] === 'K') {
+            } else if (arr[i] === 'J' || arr[i] === 'Q' || arr[i] === 'K') {
                     sum += 10;
                 } else {
                     sum += arr[i]
                 }
-            }
         }
         return [aces, sum]
     }
@@ -52,9 +51,7 @@ function blackJack() {
         return sum;
     }
 
-    const shuffleDeck = (array) => {
-        return array.map(shuffle => [Math.random(), shuffle]).sort((a, b) => a[0] - b[0]).map((a) => a[1]);
-    }
+    const shuffleDeck = (array) => array.map(shuffle => [Math.random(), shuffle]).sort((a, b) => a[0] - b[0]).map((a) => a[1])
 
 
     const drawCards = (n, person) => {
@@ -64,7 +61,7 @@ function blackJack() {
     }
 
     function sumTotal(arr) {
-        let subtotal = sumWithNoAces(arr);
+        const subtotal = sumWithNoAces(arr);
         return addAces(subtotal);
     }
 
@@ -74,13 +71,11 @@ function blackJack() {
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] === 'A') {
                 aces++;
-            } else {
-                if (arr[i] === 'J' || arr[i] === 'Q' || arr[i] === 'K') {
+            } else if (arr[i] === 'J' || arr[i] === 'Q' || arr[i] === 'K') {
                     sum += 10;
                 } else {
                     sum += arr[i]
                 }
-            }
         }
         return [aces, sum]
     }
@@ -98,7 +93,7 @@ function blackJack() {
     }
 
     const censor = (dealersHand) => {
-        let censorDeal = [];
+        const censorDeal = [];
         censorDeal[0] = '*';
         for (let i = 1; i < dealersHand.length; i += 1) {
             censorDeal.push(dealersHand[i])
@@ -114,13 +109,19 @@ function blackJack() {
         return 0;
     }
     return function (action) {
+        if (hasBeenRun === 0 && action === 'run'){
+          const newTotal = total;
+          total = 0;
+          return (newTotal > 0) ? console.log(`You've won a total of $${newTotal}!`) : console.log(`You've chosen to run from a $${newTotal*-1} bill! Yikes`)
+        }
 
         if (hasBeenRun === 0) {
 
             if (!isNaN(action)) {
                 money = action;
             } else {
-                return console.log('Bet some money you goof');
+              return console.log('Bet some money you goof!');
+
             }
 
             deck = shuffleDeck(deck);
@@ -130,10 +131,24 @@ function blackJack() {
             hasBeenRun = 1;
             money = action;
         }
+        if (action === 'run'){
+          return console.log('You can not run in the middle of a round');
+        }
 
         if (action === 'hit') {
-            drawCards(1, hand);
+          drawCards(1, hand);
+      }
+
+        if (action === 'doubledown') {
+          console.log('Doubling the bet!')
+          drawCards(1, hand);
+          money *= 2;
+          while (sumTotal(dealer) < 17) {
+            drawCards(1, dealer)
         }
+        stay = 1;
+      }
+
 
         if (action === 'stay') {
             while (sumTotal(dealer) < 17) {
@@ -142,7 +157,7 @@ function blackJack() {
             stay = 1;
         }
 
-        let checkStatus = checkBlackJack(sumTotal(dealer), sumTotal(hand));
+        const checkStatus = checkBlackJack(sumTotal(dealer), sumTotal(hand));
 
         if (checkStatus === 3) {
             console.log('This is very unusual. That is a draw. Double BlackJack!');
@@ -153,22 +168,20 @@ function blackJack() {
             console.log('Pure magic. You won BlackJack!!');
             console.log('Your hand is :', hand, ' -  sum :', sumTotal(hand))
             console.log('Dealer is :', dealer, ' -  sum :', sumTotal(dealer))
-            total = money * 2;
+            total += money * 2;
             reset();
-        } else {
-
-            if (sumTotal(hand) > 21) {
+        } else if (sumTotal(hand) > 21) {
                 console.log('You lost - Boooooh!!');
                 console.log('Your hand is :', hand, ' -  sum :', sumTotal(hand))
                 console.log('Dealer is :', dealer, ' -  sum :', sumTotal(dealer))
-                total = total - money;
+                total -= money;
                 reset();
 
             } else if (sumTotal(dealer) > 21) {
                 console.log('Wat, you won!')
                 console.log('Your hand is :', hand, ' -  sum :', sumTotal(hand))
                 console.log('Dealer is :', dealer, ' -  sum :', sumTotal(dealer))
-                total = total - money;
+                total += money;
                 reset();
             } else if (stay === 1) {
                 if (sumTotal(hand) === sumTotal(dealer)) {
@@ -181,7 +194,7 @@ function blackJack() {
                     console.log('You won!')
                     console.log('Your hand is :', hand, ' -  sum :', sumTotal(hand))
                     console.log('Dealer is :', dealer, ' -  sum :', sumTotal(dealer))
-                    total = total + money;
+                    total += money;
                     reset();
                 }
                 if (sumTotal(hand) < sumTotal(dealer)) {
@@ -189,22 +202,21 @@ function blackJack() {
                     console.log('You lost - oh no!');
                     console.log('Your hand is :', hand, ' -  sum :', sumTotal(hand))
                     console.log('Dealer is :', dealer, ' -  sum :', sumTotal(dealer))
-                    total = total - money;
+                    total -= money;
                     reset();
 
                 }
             } else {
                 console.log('Your hand is :', hand, ' -  sum :', sumTotal(hand))
                 console.log('Dealer is :', censor(dealer), ' -  sum : ? ')
-                console.log('Do you stay, hit, or reset?')
+                console.log('Do you hit, stay, or run?')
             }
-        }
     }
 }
 
-//Initialize Game
+// Initialize Game
 const play = blackJack();
 
 
 console.log('Welcome to Black Jack!');
-console.log("Initialize the game by running play()")
+console.log('Start by envoking play() <-- pass in a number as argument')
